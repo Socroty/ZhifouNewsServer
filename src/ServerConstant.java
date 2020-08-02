8f5f4ac4256be0954ac316f6226aa62521fd1a01
Socroty <socroty@gmail.com>
2020-08-02 17:54:59 +0800

    Perfect usage plan

--- src/ServerConstant.java
+++ src/ServerConstant.java
@@ -1,18 +1,18 @@
 public interface ServerConstant {
     //服务器数据请求端口
-    int post_info_port = 3011;
+    int post_info_port = 0;
     //服务器图片请求端口
-    int post_image_port = 3012;
+    int post_image_port = 0;
     //数据库地址
-    String mysql_address = "jdbc:mysql://localhost:3306/zhifounews_db?useUnicode=true&characterEncoding=utf-8&useSSL=false";
+    String mysql_address = "";
     //数据库用户名
-    String mysql_user = "root";
+    String mysql_user = "";
     //数据库密码
-    String mysql_password = "root";
+    String mysql_password = "";
     //服务器远程关闭指令
-    String listen_stop = "listen_stop";
+    String listen_stop = "";
     //图片存储文件夹路径
-    String image_local = "D:\\Projects\\AppCache\\";
+    String image_local = "";
     //数据获取请求地址
-    String api_url = "http://api01.idataapi.cn:8000/article/idataapi?KwPosition=3&appCode=qq.com&size=20&catLabel1=体育&publishDateRange=1593734400,1596326400&createDateRange=1593734400,1596326400&apikey=yoxcL1hSwUUdmeVPE0t3CdLrmQWVrCWcItCfwgfP92TSLi7L2I1ocC6YxIkBeMBg";
+    String api_url = "";
 }
